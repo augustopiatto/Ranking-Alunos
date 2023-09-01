24b9d1a1bb40d4f5e3ce0b9f37d6373ab69a2390
Augusto <ampiatto@hotmail.com>
2023-09-01 19:44:43 -0300

    banco postgresql

--- back/backend/management/commands/dev_db.py
+++ back/backend/management/commands/dev_db.py
@@ -28,9 +28,9 @@ class Command(BaseCommand):
         Aluno.objects.bulk_create(students_to_create, batch_size=50)
     
     def create_schools(self):
-        data_school = Escola.objects.create(nome="DADOS")
-        technology_school = Escola.objects.create(nome="TECNOLOGIA")
-        product_school = Escola.objects.create(nome="PRODUTO")
+        data_school = Escola.objects.create(nome="data")
+        technology_school = Escola.objects.create(nome="technology")
+        product_school = Escola.objects.create(nome="product")
 
         names = [
             "Fredo", "Roberta", "Paulo", "Tales", "Ana", "Núbia",
@@ -62,59 +62,59 @@ class Command(BaseCommand):
         lara = Aluno.objects.get(nome="Lara")
         roberto = Aluno.objects.get(nome="Roberto")
 
-        data = Escola.objects.get(nome="DADOS")
-        technology = Escola.objects.get(nome="TECNOLOGIA")
-        product = Escola.objects.get(nome="PRODUTO")
+        data = Escola.objects.get(nome="data")
+        technology = Escola.objects.get(nome="technology")
+        product = Escola.objects.get(nome="product")
 
         infos = [
             # Dados
             ## Fredo
-            {"student_name": fredo, "activity": "TAREFAS", "grade": 10, "school": data},
-            {"student_name": fredo, "activity": "DESAFIOS", "grade": 100, "school": data},
-            {"student_name": fredo, "activity": "PROJETOS", "grade": 80, "school": data},
+            {"student_name": fredo, "activity": "tasks", "grade": 10, "school": data},
+            {"student_name": fredo, "activity": "challenges", "grade": 100, "school": data},
+            {"student_name": fredo, "activity": "projects", "grade": 80, "school": data},
             ## Roberta
-            {"student_name": roberta, "activity": "TAREFAS", "grade": 100, "school": data},
-            {"student_name": roberta, "activity": "DESAFIOS", "grade": 70, "school": data},
-            {"student_name": roberta, "activity": "PROJETOS", "grade": 40, "school": data},
+            {"student_name": roberta, "activity": "tasks", "grade": 100, "school": data},
+            {"student_name": roberta, "activity": "challenges", "grade": 70, "school": data},
+            {"student_name": roberta, "activity": "projects", "grade": 40, "school": data},
             ## Paulo
-            {"student_name": paulo, "activity": "TAREFAS", "grade": 30, "school": data},
-            {"student_name": paulo, "activity": "DESAFIOS", "grade": 30, "school": data},
-            {"student_name": paulo, "activity": "PROJETOS", "grade": 30, "school": data},
+            {"student_name": paulo, "activity": "tasks", "grade": 30, "school": data},
+            {"student_name": paulo, "activity": "challenges", "grade": 30, "school": data},
+            {"student_name": paulo, "activity": "projects", "grade": 30, "school": data},
            
             # Tecnologia
             ## Paulo
-            {"student_name": paulo, "activity": "TAREFAS", "grade": 70, "school": technology},
-            {"student_name": paulo, "activity": "DESAFIOS", "grade": 80, "school": technology},
+            {"student_name": paulo, "activity": "tasks", "grade": 70, "school": technology},
+            {"student_name": paulo, "activity": "challenges", "grade": 80, "school": technology},
             ## Tales
-            {"student_name": tales, "activity": "TAREFAS", "grade": 70, "school": technology},
-            {"student_name": tales, "activity": "DESAFIOS", "grade": 60, "school": technology},
-            {"student_name": tales, "activity": "PROJETOS", "grade": 80, "school": technology},
+            {"student_name": tales, "activity": "tasks", "grade": 70, "school": technology},
+            {"student_name": tales, "activity": "challenges", "grade": 60, "school": technology},
+            {"student_name": tales, "activity": "projects", "grade": 80, "school": technology},
             ## Ana
-            {"student_name": ana, "activity": "TAREFAS", "grade": 50, "school": technology},
-            {"student_name": ana, "activity": "DESAFIOS", "grade": 20, "school": technology},
-            {"student_name": ana, "activity": "PROJETOS", "grade": 90, "school": technology},
+            {"student_name": ana, "activity": "tasks", "grade": 50, "school": technology},
+            {"student_name": ana, "activity": "challenges", "grade": 20, "school": technology},
+            {"student_name": ana, "activity": "projects", "grade": 90, "school": technology},
             ## Núbia
-            {"student_name": nubia, "activity": "TAREFAS", "grade": 10, "school": technology},
-            {"student_name": nubia, "activity": "DESAFIOS", "grade": 20, "school": technology},
-            {"student_name": nubia, "activity": "PROJETOS", "grade": 30, "school": technology},
+            {"student_name": nubia, "activity": "tasks", "grade": 10, "school": technology},
+            {"student_name": nubia, "activity": "challenges", "grade": 20, "school": technology},
+            {"student_name": nubia, "activity": "projects", "grade": 30, "school": technology},
             
             # Produto
             ## Javanil
-            {"student_name": javanil, "activity": "TAREFAS", "grade": 70, "school": product},
-            {"student_name": javanil, "activity": "DESAFIOS", "grade": 70, "school": product},
-            {"student_name": javanil, "activity": "PROJETOS", "grade": 70, "school": product},
+            {"student_name": javanil, "activity": "tasks", "grade": 70, "school": product},
+            {"student_name": javanil, "activity": "challenges", "grade": 70, "school": product},
+            {"student_name": javanil, "activity": "projects", "grade": 70, "school": product},
             ## Toelison
-            {"student_name": toelison, "activity": "TAREFAS", "grade": 30, "school": product},
-            {"student_name": toelison, "activity": "DESAFIOS", "grade": 40, "school": product},
-            {"student_name": toelison, "activity": "PROJETOS", "grade": 0, "school": product},
+            {"student_name": toelison, "activity": "tasks", "grade": 30, "school": product},
+            {"student_name": toelison, "activity": "challenges", "grade": 40, "school": product},
+            {"student_name": toelison, "activity": "projects", "grade": 0, "school": product},
             ## Lara
-            {"student_name": lara, "activity": "TAREFAS", "grade": 50, "school": product},
-            {"student_name": lara, "activity": "DESAFIOS", "grade": 80, "school": product},
-            {"student_name": lara, "activity": "PROJETOS", "grade": 50, "school": product},
+            {"student_name": lara, "activity": "tasks", "grade": 50, "school": product},
+            {"student_name": lara, "activity": "challenges", "grade": 80, "school": product},
+            {"student_name": lara, "activity": "projects", "grade": 50, "school": product},
             ## Roberto
-            {"student_name": roberto, "activity": "TAREFAS", "grade": 50, "school": product},
-            {"student_name": roberto, "activity": "DESAFIOS", "grade": 50, "school": product},
-            {"student_name": roberto, "activity": "PROJETOS", "grade": 80, "school": product},
+            {"student_name": roberto, "activity": "tasks", "grade": 50, "school": product},
+            {"student_name": roberto, "activity": "challenges", "grade": 50, "school": product},
+            {"student_name": roberto, "activity": "projects", "grade": 80, "school": product},
         ]
 
         activities_to_create = []
