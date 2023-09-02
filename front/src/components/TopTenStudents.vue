644b49c9146ccecfb44f112cf546c80819a31bce
Augusto <ampiatto@hotmail.com>
2023-09-02 01:31:14 -0300

    monta pastas para acessar a tabela correta

--- front/src/components/TopTenStudents.vue
+++ front/src/components/TopTenStudents.vue
@@ -1,22 +1,38 @@
 <template>
   <div class="top-ten-students">
     <h2>Top 10 alunos por curso</h2>
-    <v-table fixed-header>
-      <thead>
-        <tr>
-          <th>Posição</th>
-          <th>Nome do aluno</th>
-          <th>Média final</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="student in topTenStudents" :key="student.id">
-          <td>{{ student.idx }}</td>
-          <td>{{ student.name }}</td>
-          <td>{{ student.final_score }}</td>
-        </tr>
-      </tbody>
-    </v-table>
+    <div>
+      <div class="tts__buttons--container">
+        <div class="ttsb__data-button">
+          <div class="ttsb__button">Dados</div>
+          <div class="ttsb__triangle"></div>
+        </div>
+        <div class="ttsb__tech-button">
+          <div class="ttsb__button">Tecnologia</div>
+          <div class="ttsb__triangle"></div>
+        </div>
+        <div class="ttsb__product-button">
+          <div class="ttsb__button">Produto</div>
+          <div class="ttsb__triangle"></div>
+        </div>
+      </div>
+      <v-table fixed-header>
+        <thead>
+          <tr>
+            <th>Posição</th>
+            <th>Nome do aluno</th>
+            <th>Média final</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="student in topTenStudents" :key="student.id">
+            <td>{{ student.idx }}</td>
+            <td>{{ student.name }}</td>
+            <td>{{ student.final_score }}</td>
+          </tr>
+        </tbody>
+      </v-table>
+    </div>
   </div>
 </template>
 
@@ -37,5 +53,42 @@ export default {
   display: flex;
   flex-direction: column;
   gap: 16px;
+
+  .tts__buttons--container {
+    display: flex;
+    margin-bottom: -8px;
+
+    .ttsb__data-button {
+      display: flex;
+    }
+
+    .ttsb__tech-button {
+      display: flex;
+      margin-left: -16px;
+    }
+
+    .ttsb__product-button {
+      display: flex;
+      margin-left: -16px;
+    }
+
+    .ttsb__button {
+      background-color: var(--darker-blue);
+      border-radius: 4px 0 0 0;
+      padding: 4px 8px;
+      color: white;
+      max-height: 40px;
+      border-left: solid 1px black;
+      border-top: solid 1px black;
+      cursor: pointer;
+    }
+
+    .ttsb__triangle {
+      height: 0;
+      width: 0;
+      border-bottom: 40px solid var(--darker-blue);
+      border-right: 40px solid transparent;
+    }
+  }
 }
 </style>
